db2330f0e939716545b03f2ee4c8fe7256baeacc
ElevaterCPE26 <oipz2001@gmail.com>
2021-04-07 11:53:45 +0700

    update

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import React, { useState,Component } from 'react';
-import { View,Image,Text,SafeAreaView,TouchableHighlight, Button } from 'react-native';
-import WifiPresent from './wifiPresent'
-import Camera from './src/components/cameraTest'
-import Login from './src/screens/Login'
-import Calendar from './src/components/CalendarPicker'
+// import { View,Image,Text,SafeAreaView,TouchableHighlight, Button } from 'react-native';
+// import WifiPresent from './wifiPresent'
+// import Camera from './src/components/cameraTest'
+// import Login from './src/screens/Login'
+// import Calendar from './src/components/CalendarPicker'
 import MyAppNavigator from './src/index'
-import TeacherHome from './src/screens/teachers/Home'
-import StudentHome from './src/screens/students/Home'
-import Seatmap from './src/screens/students/Seatmap'
-import CheckInCamera from './src/screens/students/faceCheckInCamera'
-import Stat from './src/screens/teachers/SessionStat'
-import Create from './src/screens/teachers/SessionCreate'
-import CheckBox from './src/components/testCheckedBox'
-import WifiTest from './src/screens/teachers/settingLoc'
-import ExpoTest from './testExpo'
-import TestCamera from './CameraTest'
-import TestAPI from './testFetchAPI'
+// import TeacherHome from './src/screens/teachers/Home'
+// import StudentHome from './src/screens/students/Home'
+// import Seatmap from './src/screens/students/Seatmap'
+// import CheckInCamera from './src/screens/students/faceCheckInCamera'
+// import Stat from './src/screens/teachers/SessionStat'
+// import Create from './src/screens/teachers/SessionCreate'
+// import CheckBox from './src/components/testCheckedBox'
+// import WifiTest from './src/screens/teachers/settingLoc'
+// import ExpoTest from './testExpo'
+// import TestCamera from './CameraTest'
+// import TestAPI from './testFetchAPI'
 
-import CameraAddFaceList from './src/screens/students/CameraAddFaceList'
+// import CameraAddFaceList from './src/screens/students/CameraAddFaceList'
 
-import ActivityIndic from './testActivityIndicator'
+// import ActivityIndic from './testActivityIndicator'
 
-import Test from './graphTest'
-import WifiSurvey from './wifiSurvey'
+// import Test from './graphTest'
+// import WifiSurvey from './wifiSurvey'
 
 const App = () => {
   
--- test.py
+++
@@ -1,8 +0,0 @@
-
-score=81
-if score < 45 :
-    print('fail')
-else:
-    print('pass')
-    if(score > 80) :
-        print('with distinction')
